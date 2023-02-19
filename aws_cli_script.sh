1a23c0a8b0e94994e29308b8561a4e5a19b094de
IvanRoussev <ivan.roussev12@gmail.com>
2023-02-18 17:53:03 -0800

    Added Creating subnet 1 public 2 private region west 2a 2b

--- aws_cli_script.sh
+++ aws_cli_script.sh
@@ -11,24 +11,57 @@ RDS_PRIVATE_SUBNET_2_CIDR="10.0.3.0/24"
 
 #Regions us-west-2a & us-west-2b
 REGION_2A="us-west-2a"
-REGION_2b="us-west-2b"
+REGION_2B="us-west-2b"
 
 
 
 # _____________Start of commands________________
 
 #Creating a new VPC
-vpc_id=$(aws ec2 create-vpc --cidr-block $VPC_CIDR --region="us-west-2" --tag-specifications 'ResourceType=vpc,Tags=[{Key=Name, Value="assignment2-vpc"}]'| yq '.Vpc.VpcId')
+vpc_id=$(aws ec2 create-vpc --cidr-block $VPC_CIDR --region="us-west-2" --map-public-ip-on-launch --tag-specifications 'ResourceType=vpc,Tags=[{Key=Name, Value="assignment2-vpc"}]'| yq '.Vpc.VpcId')
 echo "VPC ID $vpc_id"
 
 
 #Create a public subnet for the ec2 instance
-public_subnet=$(
+public_subnet_2a=$(
 aws ec2 create-subnet \
 --vpc-id $vpc_id \
 --cidr-block $EC2_PUBLIC_SUBNET_CIDR \
 --availability-zone $REGION_2A \
---tag-specifications 'ResourceType=subnet, Tags=[{Key=Name, Value="ec2_public_subnet"}]' | yq '.Subnet.SubnetId'
+--tag-specifications 'ResourceType=subnet, Tags=[{Key=Name, Value="ec2_public__west_2a"}]' | yq '.Subnet.SubnetId'
 )
-echo "Subnet ID $public_subnet"
 
+
+
+#Create a private subnet for RDS west 2a
+private_subnet_2a=$(
+aws ec2 create-subnet \
+--vpc-id $vpc_id \
+--cidr-block $RDS_PRIVATE_SUBNET_1_CIDR \
+--availability-zone $REGION_2A \
+--tag-specifications 'ResourceType=subnet, Tags=[{Key=Name, Value="rds_private_west_2a"}]' | yq '.Subnet.SubnetId')
+
+#Create a private subnet for RDS west 2b
+private_subnet_2b=$(
+aws ec2 create-subnet \
+--vpc-id $vpc_id \
+--cidr-block $RDS_PRIVATE_SUBNET_2_CIDR \
+--availability-zone $REGION_2B \
+--tag-specifications 'ResourceType=subnet, Tags=[{Key=Name, Value="rds_private_west_2b"}]' | yq '.Subnet.SubnetId')
+
+
+echo "Subnet ID $public_subnet_2a"
+echo "Subnet ID $private_subnet_2a"
+echo "Subnet ID $private_subnet_2b"
+
+
+
+
+
+
+
+
+
+
+
+#Create Internet Gateway
